1d1cde05684253cbd35f8610675db553a362d014
Tomas <tomasfreire@gmail.com>
2022-08-02 22:36:10 -0300

    added enable disable func

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
   // pide conectar una address de Metamask
   async function requestAccount() {
     const account = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    console.log(account)
     return account;
   }
 
@@ -31,10 +30,9 @@ const IndexPage = () => {
         <div className="brand-logo"></div>
         <div className="brand-title">Blockdemy Token</div>
         <div className="inputs">
-          
-          {/* <input type="text" placeholder="example" /> */}
-          <button disabled={disabled} onClick={() => requestAccount().catch(err => console.error({ err }))}>Connect Wallet</button>
-          
+          <button disabled={disabled} onClick={() => requestAccount()
+            .then(() => setDisabled(true))
+            .catch(err => console.error({ err }))}>Connect Wallet</button>
           <input disabled={!disabled} type="text" placeholder="example" />
           <button disabled={!disabled} type="submit">Example</button>
         </div>
