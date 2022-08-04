261c702ee1e832de478ec2179dc171d9aa226cd8
Tomas <tomasfreire@gmail.com>
2022-08-04 14:55:34 -0300

    modified form, added chains and amount inputs

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import "../style.css"
 const IndexPage = () => {
 
   const [disabled, setDisabled] = useState();
+  const [toNetwork, setToNetwork] = useState("AVAX");
 
   // pide conectar una address de Metamask
   async function requestAccount() {
@@ -13,28 +14,43 @@ const IndexPage = () => {
   }
 
   useEffect(() => {
-    async function checkActive() {
-      const connection = await window.ethereum.selectedAddress;
-      if(connection === undefined) {
-        setDisabled(false);
-      } else {
-        setDisabled(true);
-      }
+    if (typeof window.ethereum !== 'undefined') {
+      async function checkActive() {
+        const connection = await window.ethereum._state.accounts;
+        if(connection.length === 0) {
+          setDisabled(false);
+        } else {
+          setDisabled(true);
+        }
     }
     checkActive().catch(err => console.error(err))
+    }
   }, []);
+
+  const handleOptionChange = (event) => {
+    event.target.value === "BSC" ? setToNetwork("AVAX") : setToNetwork("BSC")
+  }
 
   return (
     <main>
       <div className="container">
         <div className="brand-logo"></div>
-        <div className="brand-title">Blockdemy Token</div>
+        <div className="brand-title">Blockdemy Token Bridge</div>
         <div className="inputs">
-          <button disabled={disabled} onClick={() => requestAccount()
+        <button disabled={disabled} onClick={() => requestAccount()
             .then(() => setDisabled(true))
             .catch(err => console.error({ err }))}>Connect Wallet</button>
-          <input disabled={!disabled} type="text" placeholder="example" />
-          <button disabled={!disabled} type="submit">Example</button>
+          <form>
+            <label name="networksForm" >from:</label>
+            <select onChange={(event) => handleOptionChange(event)}>
+              <option value="BSC">BSC</option>
+              <option value="AVAX">AVAX</option>
+            </select>
+            <label>to:</label>
+            <p>{toNetwork}</p>
+          </form>
+          <input disabled={!disabled} min="0" type="number" placeholder="Amount" />
+          <button disabled={!disabled} type="submit">Call Contract</button>
         </div>
       </div>
     </main>
