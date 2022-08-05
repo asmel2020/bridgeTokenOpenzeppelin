e4fae44c3a7a4cd5e1af66125974cf891b7a3a7a
Danny Gonzalez <danny.galicia@rhizom.me>
2022-08-05 00:39:34 -0300

    d

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ethers, BigNumber } from "ethers";
+import { ethers} from "ethers";
 import detectEthereumProvider from "@metamask/detect-provider";
 // import { ethers } from "ethers";
 import "../style.css";
